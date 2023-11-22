3a02ad02318e771d64eecc111f68602ac76f68f1
GVillaf <gonzaloalejandro720@gmail.com>
2023-11-22 16:01:34 -0300

    fix: :bug: add static title and separator line to PDF document layout

--- src/components/MyDocument.jsx
+++ src/components/MyDocument.jsx
@@ -1,5 +1,5 @@
-import { Page, Text, Document, StyleSheet } from '@react-pdf/renderer';
-
+import { Page, Text, View, Document, StyleSheet, Image } from '@react-pdf/renderer';
+import logo from '../assets/images/3c-removebg-preview.png'; 
 
 const styles = StyleSheet.create({
   page: {
@@ -7,17 +7,41 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     padding: 30,
   },
-  section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1,
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  logo: {
+    width: 60, // Ajusta el tamaño según tus necesidades
+    height: 60, // Ajusta el tamaño según tus necesidades
+  },
+  line: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#666',
+    borderBottomStyle: 'solid',
+    marginBottom: 10,
+  },
+  content: {
+    fontSize: 12,
+    textAlign: 'justify',
   },
 });
 
 const MyDocument = ({ content }) => (
   <Document>
     <Page size="A4" style={styles.page}>
-      <Text style={styles.section}>{content}</Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>OKR Genius</Text>
+        <Image style={styles.logo} src={logo} />
+      </View>
+      <View style={styles.line}></View>
+      <Text style={styles.content}>{content}</Text>
     </Page>
   </Document>
 );
